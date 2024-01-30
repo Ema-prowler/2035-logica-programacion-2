f7d25dda4e7f5ef0d81b0bdac309d50d45b26b20
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-01-30 17:45:15 -0300

    aprendiendo a usar las funciones, optimizar el codigo con la misma funcion,cambiando una imagen del proyecto original por una de mi comodidad,aprendiendo el termino 'hoisting'

---
+++ app.js
@@ -0,0 +1,12 @@
+function asignarTextoElemento(elemento, texto) {
+  let elementoHTML = document.querySelector(elemento);
+  elementoHTML.innerHTML = texto;
+}
+
+function intentoDeUsuario() {
+  alert("click desde el boton");
+}
+
+asignarTextoElemento("h1", "juego del numero secreto");
+asignarTextoElemento("p", "Indica un numero del 1 al 100");
+//termino HOISTING
